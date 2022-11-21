3a6397192456c56bc674a532d3a8d931c6b124ed
KSchalau <97081280+KSchalau@users.noreply.github.com>
2022-11-21 01:44:58 -0800

    added boss hit and colliders

---
+++ Assets/BossBehaviourScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehaviourScript : MonoBehaviour {
+
+    int health = 20;
+    public Animator anim;
+    public Player player;
+    public Transform player2;
+    //public Text totalKills;
+
+    public float moveSpeed;
+
+    public float followRange;
+    private bool inRange;
+
+    void  OnTriggerEnter2D(Collider2D col) { 
+
+        if (col.gameObject.tag.Equals("Bullet")) {
+
+            if (health <= 0) {
+                //anim.SetTrigger("Dying");
+                Debug.Log("dies");
+                //player.kills++;
+                //totalKills.text = "Kills: " + player.kills;
+                Destroy(gameObject, 0.5f);
+            } else {
+                anim.SetTrigger("hit");
+                health--;
+            }
+
+        }
+
+    }
+
+    void Update () {
+        
+        if (Vector2.Distance(transform.position, player2.position) <= followRange)
+        {
+            inRange = true;
+        }
+        else
+        {
+            inRange = false;
+        }
+    }
+
+    void Start() {
+        //totalKills.text = "Kills: " + player.kills;
+    }
+
+        void FixedUpdate()
+    {
+        if (inRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player2.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
